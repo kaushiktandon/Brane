c6b9c6a90c6f28d848745e6219096169b2186b2e
kaushiktandon <kaushikt@usc.edu>
2019-05-01 19:44:46 -0700

    updated delete links

--- Delete_Links/new_delete_links.py
+++ Delete_Links/new_delete_links.py
@@ -7,6 +7,7 @@ import pandas as pd
 import sqlite3
 from sqlite3 import Error
 import operator
+import time
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -27,13 +28,16 @@ def deleteTable():
 		conn.close()
 def loadDatabase(links_file):
 	link_ids = dict()
-	deleteTable()
+	uptexts = dict()
+	downtexts = dict()
+	print("Loading links")
+	#deleteTable()
 	with open(links_file,'r+') as f:
 
-		sql = sqlite3.connect(str(database))
-		cur = sql.cursor()
+		#sql = sqlite3.connect(str(database))
+		#cur = sql.cursor()
 		#Create the data table
-		cur.execute('''CREATE TABLE IF NOT EXISTS data(link1 TEXT, link2 TEXT, link_id TEXT)''')
+		#cur.execute('''CREATE TABLE IF NOT EXISTS data(link1 TEXT, link2 TEXT, link_id TEXT, uptext TEXT, downtext TEXT)''')
 
 		numRows = 0
 		for line in f:
@@ -43,52 +47,75 @@ def loadDatabase(links_file):
 			key2 = str(data['data']['_to'])
 			id_2 = key2[6:]
 			key3 = str(data['data']['_key'])
-			cur.execute('''INSERT INTO data VALUES(?,?,?)''',(id_1,id_2,key3))
+			uptext = data['data'].get('u')
+			downtext = data['data'].get('d')
+			if(uptext != None):
+				uptext = str(uptext)
+
+			if(downtext != None):
+				downtext = str(downtext)
+
+			#cur.execute('''INSERT INTO data VALUES(?,?,?,?,?)''',(id_1,id_2,key3,uptext,downtext))
 			if((id_1,id_2) not in link_ids):
 				link_ids[id_1,id_2] = 1
+				numRows = numRows + 1
 			else:
 				link_ids[id_1,id_2] = link_ids[id_1,id_2] + 1
-			numRows = numRows + 1
+				uptexts[id_1,id_2] = uptext
+				downtexts[id_1,id_2] = downtext
 		f.close()
-		sql.commit()
-		sql.close()
-	return numRows, link_ids
+		#sql.commit()
+		#sql.close()
+	print("Loaded Links")
+	return numRows, link_ids, uptexts, downtexts
+def load_nodesJSON(nodes_file):
+	nodes = dict()
+	print ("Loading nodes")
+	with open(nodes_file) as f:
+		for line in f:
+			data = json.loads(line)
+			a_id = str(data['data']['_key'])
+			name = str(data['data']['t'])
+			nodes[a_id] = name
+	print ("Loaded nodes")
+	return nodes
 
 if __name__ == '__main__':
-	if(len(sys.argv) != 3):
-		print("USAGE: python delete_links.py [Input JSON Links File] [Name of output csv file]")
+	if(len(sys.argv) != 4):
+		print("USAGE: python new_delete_links.py [Input JSON Nodes File] [Input JSON Links File] [Name of output csv file]")
 		print("If there is a space in any of the files, put the name in quotes \"[NAME]\" ")
-		print("Example: python link_recursion.py \"All links.json\" output.csv ")
+		print("Example: python link_recursion.py nodes.json \"All links.json\" output.csv ")
 	else:
-		links_file = sys.argv[1]#'All links.json'
-		created_CSV_file = sys.argv[2]#'output_All deleted.csv'
+		nodes_file = sys.argv[1]
+		links_file = sys.argv[2]#'All links.json'
+		created_CSV_file = sys.argv[3]#'output_All deleted.csv'
 
 		count = 0.0
-		length, link_ids = loadDatabase(links_file)
+		startTime = time.time()
+		nodes = load_nodesJSON(nodes_file)
+		length, link_ids, uptexts, downtexts = loadDatabase(links_file)
+		dbTime = time.time()
 		with open(created_CSV_file, 'w+') as csvfile:
 			writer = csv.writer(csvfile,lineterminator = '\n')
 			sql = sqlite3.connect(str(database))
 			cur = sql.cursor()
 			for keyPair, value in link_ids.iteritems():
 				count = count + 1
-				if (count % 100 == 0):
+				if (count % 3000 == 0):
 					print (count * 100 / length, '%')
 			
 				if(value == 1): continue
 				
-				print keyPair
-				try:
-					queryString = "SELECT * FROM data WHERE link1=" + keyPair[0] + " AND link2=" + keyPair[1]
-					cur.execute(queryString)
-					all_rows = cur.fetchall()
-					oneToKeep = True
-					for row in all_rows:
-						if(oneToKeep):
-							oneToKeep = False
-							continue
-						else:
-							link_id = str(row[2])
-							writer.writerow(['DL',link_id])
-				except Exception as e:
-					print "Something weird happened"
-					continue
+				#print keyPair
+				writer.writerow(['DL',keyPair[0],keyPair[1]])
+				uptext = uptexts[keyPair]
+				downtext = downtexts[keyPair]
+				if(nodes.get(str(keyPair[1])) != None and nodes.get(str(keyPair[0])) != None):
+					if(uptext == None and downtext == None):
+						writer.writerow(['CL',keyPair[0],keyPair[1]])
+					else:
+						writer.writerow(['CL',keyPair[0],keyPair[1],uptext,downtext])
+		endTime = time.time()
+		print(str(endTime - startTime) + " seconds to run. ")
+		print(str(dbTime - startTime)  + " seconds to load db. ")
+		print(str(endTime - dbTime) + " seconds to find duplicates. ")
